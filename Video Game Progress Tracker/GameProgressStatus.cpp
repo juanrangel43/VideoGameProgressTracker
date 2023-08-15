f045374fddc691979165d4a031aed81c2883a1a6
juanrangel43 <juanrangel43@gmail.com>
2023-08-15 16:04:32 -0700

    Adding 1st Checkbox to Collapsible Panel

--- Video Game Progress Tracker/GameProgressStatus.cpp
+++ Video Game Progress Tracker/GameProgressStatus.cpp
@@ -8,29 +8,35 @@ GameProgressStatus::GameProgressStatus(wxFrame *parent)
 {
 	mpGPS = this;
 
+	mpMainProgress = new wxCollapsiblePane(mpGPS, wxID_ANY, "Main", wxDefaultPosition, wxDefaultSize);
+	pCollapsPaneWin = mpMainProgress->GetPane();
+
+	mpCB = new wxCheckBox(pCollapsPaneWin, ID_CHECKBOX, wxT("Ch. 1"), wxPoint(150, 150));
+
 	ProgressDisplay();
-	//StatusBar();
+	StatusBar();
+	MainProgCheckBoxProperties();
 }
 
 
 void GameProgressStatus::ProgressDisplay()
 {
+	/*
 	wxCollapsiblePane *mpMainProgress = new wxCollapsiblePane(this, wxID_ANY, "Main", wxDefaultPosition, wxDefaultSize);
-	wxWindow *win = mpMainProgress->GetPane();
-	
+	pCollapsPaneWin = mpMainProgress->GetPane();
+	*/
 	wxBoxSizer *mainProgressBS = new wxBoxSizer(wxHORIZONTAL);
 	wxBoxSizer *mainProgressSubBS = new wxBoxSizer(wxVERTICAL);
-	mainProgressBS->AddSpacer(20);
+	mainProgressBS->AddSpacer(35);
 	mainProgressBS->Add(mainProgressSubBS, 1);
 
-	mainProgressSubBS->Add(new wxStaticText(win, -1, "Static text"), 0, wxALIGN_LEFT | wxALL, 3);
+	mainProgressSubBS->Add(new wxStaticText(pCollapsPaneWin, -1, "Static text"),
+		0, wxALIGN_LEFT | wxALL, 3);
+	mainProgressSubBS->Add(mpCB);
 
-	wxPanel *statusBar = new wxPanel(win);
-	//statusBar->Connect(wxEVT_PAINT, wxPaintEventHandler(GameProgressStatus::OnPaintDrawStatus));
-
+	wxPanel *statusBar = new wxPanel(pCollapsPaneWin);
 	mainProgressSubBS->Add(statusBar, 0, wxALIGN_LEFT | wxALL, 3);
-
-	win->SetSizer(mainProgressBS);
+	pCollapsPaneWin->SetSizer(mainProgressBS);
 }
 
 
@@ -41,12 +47,38 @@ void GameProgressStatus::StatusBar()
 }
 
 
+// Placeholder for actual status bar
+// Last checked: 8/15/23
 void GameProgressStatus::OnPaintDrawStatus(wxPaintEvent &event)
 {
 	wxPaintDC dc(mpGPS);
 
-	wxCoord x1 = 50, y1 = 60;
-	wxCoord x2 = 190, y2 = 60;
+	wxCoord x1 = 50, y1 = 300;
+	wxCoord x2 = 190, y2 = 300;
 
 	dc.DrawLine(x1, y1, x2, y2);
+}
+
+
+void GameProgressStatus::MainProgCheckBoxProperties()
+{
+	mpCB->SetValue(true);
+
+	Connect(ID_CHECKBOX, wxEVT_COMMAND_CHECKBOX_CLICKED,
+		wxCommandEventHandler(GameProgressStatus::OnToggle));
+}
+
+
+void GameProgressStatus::OnToggle(wxCommandEvent &event)
+{
+
+	if (mpCB->GetValue())
+	{
+		this->Show(true);
+	}
+
+	else 
+	{
+		this->Show(false);
+	}
 }
--- Video Game Progress Tracker/GameProgressStatus.h
+++ Video Game Progress Tracker/GameProgressStatus.h
@@ -17,11 +17,19 @@ public:
 	void StatusBar();
 	void OnPaintDrawStatus(wxPaintEvent &event);
 
+	void MainProgCheckBoxProperties();
+	void OnToggle(wxCommandEvent &event);
+
 
 private:
 	wxPanel *mpGPS;
+
+	wxWindow *pCollapsPaneWin;
+	wxCollapsiblePane *mpMainProgress;
+	wxCheckBox *mpCB;
 };
 
+const int ID_CHECKBOX = 100;
 
 
 #endif
